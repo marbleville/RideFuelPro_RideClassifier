8419b6b7c07b6186674a63699529e0d52a573fee
marbleville <allen.ehrhardt@gmail.com>
2024-04-30 16:08:25 -0400

    refactores getNumActivities

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const fs = require("fs");
 const api = require("./stravaAPI.js");
 
 /**
- * Writes num activities to the specified file
+ * Retuns an array of num activities
  * @param {number} num - Number of activities to write
- * @param {string} fileName - Name of file to write to
+ * @returns {Array} - Array of num activites
  */
-async function writeActivites(num, fileName) {
+async function getNumActivites(num) {
 	let perPage = 10;
 	let numPages = num % perPage;
 
@@ -18,15 +18,7 @@ async function writeActivites(num, fileName) {
 		console.log(`Page ${i} fetched`);
 	}
 
-	try {
-		fs.writeFileSync(
-			`../resources/${fileName}.json`,
-			JSON.stringify(activities)
-		);
-		console.log(`${activities.length} Activities written to file`);
-	} catch (error) {
-		console.error(error);
-	}
+	return activities;
 }
 
-writeActivites(10, "activities");
+getNumActivites(10, "activities");
