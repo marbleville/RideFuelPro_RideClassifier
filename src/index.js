62c10b417d4640b29f240914029286ae1349988a
marbleville <allen.ehrhardt@gmail.com>
2024-05-01 09:54:37 -0400

    added main and skeleton functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 const fs = require("fs");
 const api = require("./stravaAPI.js");
 
+// Object contianing the different types of rides
 const typeOfRide = {
 	intervals: "Intervals",
 	endurance: "Endurance",
 	race: "Race",
+	unknown: "Unknown",
+};
+
+// Object containing a ride's assiciated data
+let rideEntry = {
+	name: "",
+	distance: 0,
+	moving_time: 0,
+	total_elevation_gain: 0,
+	percent_up: 0,
+	percent_down: 0,
+	percent_flat: 0,
+	average_uphill_gradient: 0,
+	average_speed: 0,
+	average_watts: 0,
+	weighted_average_watts: 0,
+	kilojoules: 0,
+	average_heartrate: 0,
+	power_stream: [],
+	altitude_stream: [],
+	workout_type: "",
 };
 
 /**
  * Retuns an array of num activities
- * @param {number} num - Number of cycling activities to write
+ * @param {Number} num - Number of cycling activities to write
  * @returns {Array} - Array of num activites
  */
 async function getNumActivites(num) {
@@ -33,7 +55,6 @@ async function getNumActivites(num) {
 		page++;
 	}
 
-	console.log(activities);
 	return activities;
 }
 /**
@@ -43,26 +64,40 @@ async function getNumActivites(num) {
  * @returns {Array} - Array of refactored rides
  */
 function refactorRides(rides) {
-	let rideEntry = {
-		name: "",
-		distance: 0,
-		moving_time: 0,
-		total_elevation_gain: 0,
-		percent_up: 0,
-		percent_down: 0,
-		percent_flat: 0,
-		average_uphill_gradient: 0,
-		average_speed: 0,
-		average_watts: 0,
-		weighted_average_watts: 0,
-		kilojoules: 0,
-		average_heartrate: 0,
-		power_stream: [],
-		altitude_stream: [],
-		workout_type: "",
-	};
-
 	let refactoredRides = [];
 }
 
+/**
+ * Classfies an array of rides into different types
+ *
+ * @param {Array} rides - Array of rides to be classified
+ * @param {Number} ftp - Rider's FTP
+ * @returns {Array} - Array of classified rides
+ */
+function getRideTypeArray(rides, ftp) {}
+
+/**
+ * Returns the type of ride based on the rideEntry object
+ *
+ * @param {rideEntry} ride - Ride object to be classfied
+ * @param {Number} ftp - Rider's FTP
+ * @returns {String} - Ride type
+ */
+function getRideType(ride, ftp) {}
+
+/**
+ * Main function for the classifier script
+ *
+ * @returns {Number} - 1 on error, 0 on success
+ */
+function main() {
+	// Get the command line arguments.
+	const args = process.argv.slice(2);
+
+	// Check if the correct number of arguments were passed.
+	if (args.length !== 2) {
+		console.log("Usage: node index.js [num_activities] [rider_ftp]");
+		return 1;
+	}
+}
 getNumActivites(10, "activities");
