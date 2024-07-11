2323f261cd6d0fab1b61c90ed2341c25686b0b95
marbleville <allen.ehrhardt@gmail.com>
2024-07-11 15:32:14 -0400

    refacotring

--- src/rideClassifier.js
+++ src/rideClassifier.js
@@ -1,62 +1,7 @@
 const { id } = require("vega");
 var LowpassFilter = require("lowpassf");
 
-// Object contianing the different types of rides
-const typeOfRide = {
-	intervals: "Intervals",
-	endurance: "Endurance",
-	race: "Race",
-	unknown: "Unknown",
-};
-
-// Object containing a hill's associated data
-const hillEntry = {
-	idxStart: 0,
-	idxEnd: 0,
-	distance: 0,
-	elevationGain: 0,
-	averageGradient: 0,
-	averageSpeed: 0,
-	averageWatts: 0,
-};
-
-const intervalEntry = {
-	idxStart: 0,
-	idxEnd: 0,
-	time: 0,
-	averageWatts: 0,
-};
-
-// Object containing a ride's assiciated data
-let rideEntry = {
-	name: "",
-	distance: 0,
-	moving_time: 0,
-	total_elevation_gain: 0,
-	percent_up: 0,
-	percent_down: 0,
-	percent_flat: 0,
-	average_uphill_gradient: 0,
-	average_speed_uphill: 0,
-	average_speed_downhill: 0,
-	average_speed_flat: 0,
-	total_elevation_gain: 0,
-	average_speed: 0,
-	average_watts: 0,
-	average_watts_uphill: 0,
-	average_watts_downhill: 0,
-	average_watts_flat: 0,
-	weighted_average_watts: 0,
-	kilojoules: 0,
-	average_heartrate: 0,
-	power_stream: [],
-	altitude_stream: [],
-	distance_stream: [],
-	time_stream: [],
-	hills: [],
-	intervals: [],
-	workout_type: "",
-};
+import { typeOfRide, hillEntry, intervalEntry, rideEntry } from ".types";
 
 /**
  * Returns the type of ride based on the rideEntry object
