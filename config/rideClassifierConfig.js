1300178001e12d1d16caa481a38d0634d988531b
marbleville <allen.ehrhardt@gmail.com>
2024-07-24 14:07:41 -0400

    draft of classifier done

---
+++ config/rideClassifierConfig.js
@@ -0,0 +1,5 @@
+export default {
+	minTotalIntervalTime: 300, // the minimum total time in seconds for intervals to be considered a interval ride
+	enduranceFTPThresholdCoefficient: 0.75, // the coefficient to multiply the FTP by to determine the threshold for an endurance ride
+	minAverageIntervalTime: 300, // the minimum average time in seconds to differentiate and interval ride form a race
+};
--- src/rideClassifier.js
+++ src/rideClassifier.js
@@ -1,4 +1,5 @@
 import { typeOfRide, rideEntry } from "./types.js";
+import config from "../config/rideClassifierConfig.js";
 
 /**
  * Returns the type of ride based on the rideEntry object
@@ -6,8 +7,24 @@ import { typeOfRide, rideEntry } from "./types.js";
  * @param {rideEntry} ride - Ride object to be classfied
  * @param {Number} ftp - Rider's FTP
  *
- * @returns {String} - Ride type
+ * @returns {typeOfRide} - Ride type
  */
 export function getRideType(ride, ftp) {
-	return typeOfRide.unknown;
+	let totalIntervalTime = ride.intervals.reduce(
+		(acc, interval) => acc + interval.time,
+		0
+	);
+
+	let averageIntervalTime = totalIntervalTime / ride.intervals.length;
+
+	if (totalIntervalTime < config.minTotalIntervalTime) {
+		return typeOfRide.endurance;
+	} else if (
+		ride.average_watts > ftp * config.enduranceFTPThresholdCoefficient &&
+		averageIntervalTime < config.minAverageIntervalTime
+	) {
+		return typeOfRide.race;
+	} else {
+		return typeOfRide.intervals;
+	}
 }
